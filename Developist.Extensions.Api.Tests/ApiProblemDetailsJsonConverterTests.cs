d18cf0836bc6b0f4cdd3cf4e435ac01051eb0a5a
jimatas <atask@hr.nl>
2022-10-02 21:00:48 +0200

    Unit tests for ApiProblemDetailsJsonConverter.

---
+++ Developist.Extensions.Api.Tests/ApiProblemDetailsJsonConverterTests.cs
@@ -0,0 +1,100 @@
+﻿using Developist.Extensions.Api.ProblemDetails;
+
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Developist.Extensions.Api.Tests
+{
+    [TestClass]
+    public class ApiProblemDetailsJsonConverterTests
+    {
+        [TestMethod]
+        public void Serialize_GivenDefaultProblemDetailsInstance_WritesEmptyObject()
+        {
+            ApiProblemDetails problemDetails = new();
+
+            var jsonString = JsonSerializer.Serialize(problemDetails);
+
+            Assert.IsNotNull(jsonString);
+            Assert.AreEqual("{}", jsonString);
+        }
+
+        [TestMethod]
+        public void Serialize_GivenProblemDetailsWithAllPropertiesSet_WritesAllProperties()
+        {
+            var problemDetails = new ApiProblemDetails
+            {
+                Type = new Uri("https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500"),
+                Title = "Internal Server Error",
+                Status = HttpStatusCode.InternalServerError,
+                Detail = "There was a problem servicing your request.",
+                Instance = new Uri("https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500")
+            };
+
+            var jsonString = JsonSerializer.Serialize(problemDetails);
+
+            Assert.IsNotNull(jsonString);
+            Assert.AreEqual("{\"type\":\"https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500\",\"title\":\"Internal Server Error\",\"status\":500,\"detail\":\"There was a problem servicing your request.\",\"instance\":\"https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500\"}", jsonString);
+        }
+
+        [TestMethod]
+        public void Serialize_GivenProblemDetailsWithSingleSimpleExtensionProperty_WritesThatProperty()
+        {
+            ApiProblemDetails problemDetails = new();
+            problemDetails.Extensions["customErrorData"] = "Additional information about the error.";
+
+            var jsonString = JsonSerializer.Serialize(problemDetails);
+
+            Assert.IsNotNull(jsonString);
+            Assert.AreEqual("{\"customErrorData\":\"Additional information about the error.\"}", jsonString);
+        }
+
+        [TestMethod]
+        public void Serialize_GivenProblemDetailsWithComplexExtensionProperty_WritesThatProperty()
+        {
+            ApiProblemDetails problemDetails = new();
+            problemDetails.Extensions["customErrorData"] = new CustomErrorData1
+            {
+                ErrorNumber = 100,
+                ErrorInformation = "Additional information about the error."
+            };
+
+            var jsonString = JsonSerializer.Serialize(problemDetails);
+
+            Assert.IsNotNull(jsonString);
+            Assert.AreEqual("{\"customErrorData\":{\"ErrorNumber\":100,\"ErrorInformation\":\"Additional information about the error.\"}}", jsonString);
+        }
+
+        class CustomErrorData1
+        {
+            public int ErrorNumber { get; set; }
+            public string? ErrorInformation { get; set; }
+        }
+
+        [TestMethod]
+        public void Serialize_GivenProblemDetailsWithComplexExtensionPropertyWithCustomNaming_WritesThatProperty()
+        {
+            ApiProblemDetails problemDetails = new();
+            problemDetails.Extensions["customErrorData"] = new CustomErrorData2
+            {
+                ErrorNumber = 100,
+                ErrorInformation = "Additional information about the error."
+            };
+
+            var jsonString = JsonSerializer.Serialize(problemDetails);
+
+            Assert.IsNotNull(jsonString);
+            Assert.AreEqual("{\"customErrorData\":{\"error_no\":100,\"error_info\":\"Additional information about the error.\"}}", jsonString);
+        }
+
+        class CustomErrorData2
+        {
+            [JsonPropertyName("error_no")]
+            public int ErrorNumber { get; set; }
+
+            [JsonPropertyName("error_info")]
+            public string? ErrorInformation { get; set; }
+        }
+    }
+}
