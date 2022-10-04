baefba7d746c16e3bae770e7fa2b9581ddf454a1
jimatas <atask@hr.nl>
2022-10-04 13:03:58 +0200

    Added some more ApiException subclasses for common HTTP error codes.

---
+++ Developist.Extensions.Api/Exceptions/TooManyRequestsException.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace Developist.Extensions.Api.Exceptions
+{
+    public class TooManyRequestsException : ApiException
+    {
+        public TooManyRequestsException()
+            : base(HttpStatusCode.TooManyRequests) { }
+
+        public TooManyRequestsException(string message)
+            : base(HttpStatusCode.TooManyRequests, message) { }
+
+        public TooManyRequestsException(string message, Exception innerException)
+            : base(HttpStatusCode.TooManyRequests, message, innerException) { }
+
+        public TimeSpan? RetryAfter { get; set; }
+    }
+}
